a07757962f8f0f528465ea0a81cd2969ad400bf3
pawanpn <pudasaini.pawan23@gmail.com>
2026-04-16 13:29:33 +0545

    feat: initialize Tauri backend with staff and device registration commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,13 @@
 use serde_json::Value;
 
 #[tauri::command]
-pub async fn register_new_staff(employee: Value) -> Result<(), String> {
+pub async fn register_new_staff_unique(employee: Value) -> Result<(), String> {
     println!("Staff Sync: {:?}", employee);
     Ok(())
 }
 
 #[tauri::command]
-pub async fn register_new_device(device: Value) -> Result<(), String> {
+pub async fn register_new_device_unique(device: Value) -> Result<(), String> {
     println!("Device Sync: {:?}", device);
     Ok(())
 }
@@ -17,8 +17,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            register_new_staff,
-            register_new_device
+            register_new_staff_unique,
+            register_new_device_unique
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
